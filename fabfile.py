085210a063750aa4e03e7fa9daa0f3ea6f1240c2
Dovydas Gulbinas <dovydasgulbinas@gmail.com>
2018-06-06 13:36:52 +0300

    fixing css

--- fabfile.py
+++ fabfile.py
@@ -2,11 +2,16 @@ from fabric.api import local
 from fabric.context_managers import lcd
 from fabric.state import env
 import os
-env.blogdir = "/Users/hermes/Desktop/blog"
-env.draftdir = os.path.join(env.blogdir, '_drafts')
-env.postdir = os.path.join(env.blogdir, '_posts')
-env.jekyll_port = 4000
-env.remote_url = "https://megamorphf.github.io"
+
+BLOGDIR = "/Users/hermes/Desktop/blog"
+FILENAME_DATE_FORMAT = '%Y-%m-%d-'
+JEKYLL_PORT = 4000
+REMOTE_URL = "https://megamorphf.github.io"
+LOCAL_URL = "http://127.0.0.1:{}".format(JEKYLL_PORT)
+
+DRAFTDIR = os.path.join(BLOGDIR, '_drafts')
+POSTDIR = os.path.join(BLOGDIR, '_posts')
+
 
 """
 conda create -n deploy python=2.7 fabric
@@ -18,32 +23,69 @@ alias blog-edit="cd $blogdir && vifm ."
 """
 
 
-def serve():
-    with lcd(env.blogdir):
-        _open_in_browser("http://127.0.0.1:{}".format(env.jekyll_port))
+def serve(blogdir=BLOGDIR, local_url=LOCAL_URL):
+    with lcd(blogdir):
+        _open_in_browser(local_url)
         local("bundle exec jekyll serve --drafts")
+
 
 #### HELPERS ####
 
-def list_drafts(draftdir=env.draftdir, postdir=env.postdir):
+
+def list_drafts(draftdir=DRAFTDIR, msg='select'):
     from os import listdir
     from os.path import isfile, join
     drafts = [f for f in listdir(draftdir) if isfile(join(draftdir, f))]
     drafts.append ('[Chose no file]')
-    drafts_enumerated = enumerate(drafts, start=1)
+    draftfile = None
 
-    for i, draft in drafts_enumerated:
+    for i, draft in enumerate(drafts, start=1):
         print("    {}. {}".format(i, draft))
-    number = input("Choose draft you want to publish: ")
+    number = int(input("Choose draft you want to {}: ".format(msg)))
 
     if number  == len(drafts):  # chose last item, skip
         print "Skipping file choice"
-        return
+        return None
 
-    for i, draft in drafts_enumerated:
+    for i, draft in enumerate(drafts, start=1):
+        print number, i, draft
         if i == number:
             draftfile = os.path.join(draftdir, draft)
-            local('mv {} {}'.format(draftfile, postdir))
+            return draftfile
+
+
+def pub_draft(draftdir=DRAFTDIR, postdir=POSTDIR,
+        dateformat=FILENAME_DATE_FORMAT):
+
+    sfile = list_drafts(draftdir, 'publish')
+
+    if not sfile:
+        print "File you have chosen does not exist"
+        return None
+
+    timestr = _timenow(dateformat)
+    origname = os.path.basename(sfile)
+    newname = timestr + origname
+    pubpath = (os.path.join(postdir, newname))
+    local('mv {} {}'.format(sfile, pubpath))
+    return pubpath
+
+
+def publish(draftdir=DRAFTDIR, postdir=POSTDIR, pageurl=REMOTE_URL):
+    """lets you to choose a file for publishing"""
+    local("git checkout master")
+    local("git pull origin master")
+    fpath = pub_draft(draftdir, postdir)
+    local('git add {}'.format(fpath))
+    local('git commit')
+    local("git push origin master")
+    _open_in_browser(pageurl)
+
+
+def edit_draft():
+    sfile = list_drafts(DRAFTDIR, 'editing')
+    local('vim {}'.format(sfile))
+
 
 def commit():
     local("git status")
@@ -61,16 +103,6 @@ def push():
     local("git push origin master")
 
 
-def publish():
-    """does a commit and pushes to master"""
-    local("git pull origin master")
-    list_drafts()
-    commit()
-    local("git checkout master")
-    local("git push origin master")
-    _open_in_browser("https://megamorphf.github.io")
-
-
 def _user_confirms(message):
     # raw_input returns the empty string for "enter"
     yes = {'yes', 'y', 'ye', ''}
@@ -84,6 +116,13 @@ def _user_confirms(message):
     else:
         print("Please respond with 'yes' or 'no'")
 
+
+## UTILS ##
+
+
+def _timenow(dateformat=FILENAME_DATE_FORMAT):
+    import datetime
+    return datetime.datetime.now().strftime(dateformat)
 
 def _open_in_browser(url):
     import subprocess
