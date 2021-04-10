d648fc4f4e7260becd5318d54d70061691496203
Dovydas Gulbinas <dovydasgulbinas@gmail.com>
2021-04-10 20:40:00 +0300

    Removing bloated CLI class

--- migrator.py
+++ migrator.py
@@ -175,7 +175,6 @@ def transform(note_path, src_assets_dir="", dst_assets_dir=""):
     file_ = transform_singleline(file_)
     file_ = transform_multiline(file_)
     file_ = transform_links(file_)
-    # file_ = transform_assets_paths(file_, src_assets_dir, dst_assets_dir)
 
     return file_.getvalue()
 
@@ -237,64 +236,6 @@ def git_initial_setup(*, default_branch, tag_name, migration_branch, posts_dir, 
     ]
     _process_cmd_queue(cmd_queue)
 
-
-class CLI:
-    def __init__(self):
-        parser = argparse.ArgumentParser(
-            description="Allows migrating or adapting existing notes for Blogit",
-            usage="""jegit.py <command> [<args>]
-
-Commands:
-   migrate\tMigrates from Jekyll to Blogit
-   transform\tDoes Inline Transform for allready created and committed notes
-""",
-        )
-        parser.add_argument("command", help="Subcommand to run")
-        # parse_args defaults to [1:] for args, but you need to
-        # exclude the rest of the args too, or validation will fail
-        args = parser.parse_args(sys.argv[1:2])
-        if not hasattr(self, args.command):
-            print("Unrecognized command")
-            parser.print_help()
-            exit(1)
-        # use dispatch pattern to invoke method with same name
-        getattr(self, args.command)()
-
-    def migrate(self):
-        parser = argparse.ArgumentParser(
-            description="Run all necessary steps for migration to Blogit"
-        )
-
-        parser.add_argument("-i", "--posts_dir", default="./_posts", type=pathlib.Path)
-        parser.add_argument(
-            "-o", "--articles_dir", default="./articles", type=pathlib.Path
-        )
-        parser.add_argument("-n", "--no_blogit_init", action="store_true")
-        parser.add_argument("-d", "--default_branch", type=str, default="master")
-        parser.add_argument(
-            "-t", "--tag_name", type=str, default="before_migrating_to_blogit"
-        )
-        parser.add_argument(
-            "-m", "--migration_branch", type=str, default="blogit_migration_branch"
-        )
-
-        # inside a subcommand, ignore the first two args
-        args = parser.parse_args(sys.argv[2:])
-
-        if not args.posts_dir.exists() and not args.posts_dir.is_dir():
-            raise Exception(
-                f"Path '{args.posts_dir}' provided is not a directory or does not exist"
-            )
-
-        print(args.posts_dir, args.articles_dir, args.no_blogit_init)
-        git_initial_setup(
-            default_branch=args.default_branch,
-            tag_name=args.tag_name,
-            migration_branch=args.migration_branch,
-        )
-
-    def transform(self):
-        print("Not Implemented")
 
 def main():
     parser = argparse.ArgumentParser(
